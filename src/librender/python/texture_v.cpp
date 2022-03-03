41220e59065b565eb0d32bb482d832d46a5ee0ba
Peter Kocsis <peter.kocsis@tum.de>
2022-03-03 12:37:35 +0100

    Custom texture plugin

--- src/librender/python/texture_v.cpp
+++ src/librender/python/texture_v.cpp
@@ -1,10 +1,74 @@
 #include <mitsuba/render/interaction.h>
 #include <mitsuba/render/texture.h>
+#include <mitsuba/core/properties.h>
 #include <mitsuba/python/python.h>
 
+
+/// Trampoline for derived types implemented in Python
+MTS_VARIANT class PyTexture : public Texture<Float, Spectrum> {
+public:
+    MTS_IMPORT_BASE(Texture, m_id)
+    MTS_IMPORT_TYPES(Texture)
+
+    PyTexture(const Properties &props) : Texture(props) { }
+
+    UnpolarizedSpectrum eval(const SurfaceInteraction3f &si,
+                             Mask active = true) const override {
+        using Return = UnpolarizedSpectrum;
+        PYBIND11_OVERLOAD_PURE(Return, Texture, eval, si, active);
+    }
+
+    std::pair<Wavelength, UnpolarizedSpectrum>
+    sample_spectrum(const SurfaceInteraction3f &si,
+                    const Wavelength &sample,
+                    Mask active = true) const override {
+        using Return = std::pair<Wavelength, UnpolarizedSpectrum>;
+        PYBIND11_OVERLOAD_PURE(Return, Texture, sample_spectrum, si, sample, active);
+    }
+
+    Wavelength pdf_spectrum(const SurfaceInteraction3f &si,
+                            Mask active = true) const override {
+        using Return = Wavelength;
+        PYBIND11_OVERLOAD_PURE(Return, Texture, pdf_spectrum, si, active);
+    }
+
+    std::pair<Point2f, Float> sample_position(const Point2f &sample,
+                                              Mask active = true) const override {
+        using Return = std::pair<Point2f, Float>;
+        PYBIND11_OVERLOAD_PURE(Return, Texture, sample_position, sample, active);
+    }
+
+    ScalarFloat mean() const override {
+        using Return = ScalarFloat;
+        PYBIND11_OVERLOAD_PURE(Return, Texture, mean);
+    }
+
+    std::string to_string() const override {
+        using Return = std::string;
+        PYBIND11_OVERLOAD_PURE(Return, Texture, to_string);
+    }
+
+    /*Float pdf_position(const Point2f &p, Mask active = true) const override {
+        PYBIND11_OVERLOAD_PURE(Float, Texture, pdf_position, p, active);
+    }
+
+
+    Float eval_1(const SurfaceInteraction3f &si, Mask active = true) const override {
+        PYBIND11_OVERLOAD_PURE(Float, Texture, eval_1, si, active);
+    }*/
+};
+
 MTS_PY_EXPORT(Texture) {
-    MTS_PY_IMPORT_TYPES(Texture)
-    MTS_PY_CLASS(Texture, Object)
+    MTS_PY_IMPORT_TYPES()
+    using PyTexture = PyTexture<Float, Spectrum>;
+
+    auto texture = py::class_<Texture, PyTexture, ref<Texture>>(m, "Texture", D(Texture))
+        .def(py::init<const Properties&>())
+        .def_readwrite("m_id", &PyTexture::m_id);
+
+    MTS_PY_REGISTER_OBJECT("register_texture", Texture)
+
+    /*MTS_PY_CLASS(Texture, Object)
         .def_static("D65", &Texture::D65, "scale"_a = 1.f)
         .def("mean", &Texture::mean, D(Texture, mean))
         .def("is_spatially_varying", &Texture::is_spatially_varying,
@@ -32,7 +96,10 @@ MTS_PY_EXPORT(Texture) {
         .def("pdf_position",
             vectorize(&Texture::pdf_position),
             "p"_a, "active"_a = true, D(Texture, pdf_position));
+}*/
 }
+
+
 
 MTS_PY_EXPORT(Volume) {
     MTS_PY_IMPORT_TYPES(Volume)
